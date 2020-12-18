420f09dc14642836416c1346322ab8e1b0e33218
Lewis Masatu Wanjara <lewicpro@gmail.com>
2020-12-18 10:23:04 +0300

    Create urls.py

---
+++ webtoolproj/api/urls.py
@@ -0,0 +1,19 @@
+from .views import *
+from django.urls import path, include
+# from rest_framework impauth_tokenort routers, viewsets
+from rest_framework.authtoken.views import obtain_auth_token
+
+# Additionally, we include login URLs for the browsable API.
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+    path('Requests/', RequestView.as_view(), name='requestst'),
+    path('login/', obtain_auth_token, name='login'),
+    path('Register/', CreateUserView.as_view(), name='nubergen'),
+    path('Domains/', SearchedDomainView.as_view(), name='domain'),
+    # path('Domains/:<str:passed>/', SearchedDomainView.as_view(), name='domain'),
+]
